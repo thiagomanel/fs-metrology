4a1f0691e3a00707051e7c457134bfcc19808775
Thiago Manel <thiago.manel@gmail.com>
2018-05-24 10:56:36 -0300

    add skeleton for trace capture experiments

---
+++ experiments/capture-stap/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Any setup required by the experiment goes here. Things like installing
+# packages, allocating resources or deploying software on remote
+# infrastructure can be implemented here.
+set -e
+
+# add commands here:
+
+exit 0
---
+++ experiments/capture-strace/post-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This file should contain the series of steps that are required to execute 
+# the experiment. Any non-zero exit code will be interpreted as a failure
+# by the 'popper check' command.
+set -e
+
+# add commands here:
+
+exit 0
