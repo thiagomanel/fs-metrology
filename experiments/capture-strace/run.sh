6589d87134c808a69ffd45b121aea188968d07bc
thiagomanel <thiago.manel@gmail.com>
2019-03-07 09:43:55 -0300

    implement run script for strace method

--- experiments/capture-strace/run.sh
+++ experiments/capture-strace/run.sh
@@ -4,6 +4,24 @@
 # by the 'popper check' command.
 set -e
 
-# add commands here:
+#force deleting any previous results
+#rm -rf results/baseline_output
+#mkdir -p results/baseline_output
+
+env_fullpath=$(realpath environment.conf)
+
+#invoke the exp
+tool="strace"
+for wld in seqr rr
+do
+    for nfg in 1 2 3 4
+    do 
+        for i in `seq 1 50`
+        do
+            echo "run sample" $i
+            ../utils/bin/coordinator --env $env_fullpath -w $wld --nforeground $nfg --nbackground 0 --trace_tool $tool --bsize 4096
+        done
+    done
+done
 
 exit 0
