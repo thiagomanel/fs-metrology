c8fef7e6824d914e00c40e7a0a00d5f9d89feb91
Thiago Manel <thiago.manel@gmail.com>
2019-08-02 14:16:53 -0300

    organize bpftrace code

---
+++ experiments/capture-bpftrace/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This file should contain the series of steps that are required to execute 
+# the experiment. Any non-zero exit code will be interpreted as a failure
+# by the 'popper check' command.
+set -e
+
+#force deleting any previous results
+#rm -rf results/baseline_output
+#mkdir -p results/baseline_output
+
+env_fullpath=$(realpath environment.conf)
+
+#invoke the exp
+tool="strace"
+for wld in seqr rr
+do
+    #for nfg in 1 2 3 4
+    for nfg in 1
+    do 
+        for i in `seq 1 1`
+        do
+            echo "run sample" $i
+            ../utils/bin/coordinator --env $env_fullpath -w $wld --nforeground $nfg --nbackground 0 --trace_tool $tool --bsize 4096
+        done
+    done
+done
+
+exit 0
---
+++ experiments/capture-bpftrace/setup/local.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+env=$1
+source $env
+
+#get the code
+rm -rf $WORKLOADGEN_BASEDIR/fs-microbench
+#I was using an old linux box that failed to curl and wget from github because was using an outdated openssl
+git clone https://github.com/thiagomanel/fs-microbench.git $WORKLOADGEN_BASEDIR/fs-microbench
+
+#compile the code
+make -C $WORKLOADGEN_BASEDIR/fs-microbench/src/
+
+#configure the tests
+#TODO: do we really need it or just drop values at run?
