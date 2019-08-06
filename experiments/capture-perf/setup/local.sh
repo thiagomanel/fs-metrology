08cba18fc0fe63964254e1deb25d92c7c4a26046
Daniel Mitre <danielmitre@lsd.ufcg.edu.br>
2019-08-06 09:43:12 -0300

    Installs linux-tools to ensure perf is installed

---
+++ experiments/capture-perf/setup/local.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+env=$1
+source $env
+
+# install from repository
+sudo apt-get update
+sudo apt-get install -y linux-tools-common
+
+#configure the tests
+#TODO: do we really need it or just drop values at run?
